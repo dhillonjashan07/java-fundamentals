523fa8adbc23971369cfe0d06bd7042e1c2b37fc
dhillonjashan07 <102655916+dhillonjashan07@users.noreply.github.com>
2025-02-28 09:44:06 +0530

    Add files via upload

---
+++ volOfCyl.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class volOfCyl {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("Enter the radius of the cylinder: ");
+        double radius = sc.nextDouble();
+
+        System.out.print("Enter the height of the cylinder: ");
+        double height = sc.nextDouble();
+
+        
+        double volume = Math.PI * Math.pow(radius, 2) * height;
+
+        
+        System.out.println("The volume of the cylinder is: " + volume);
+
+        sc.close();
+    }
+}
